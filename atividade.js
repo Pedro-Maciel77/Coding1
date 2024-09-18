fb56309a84d3f10b012ed51a2a83adab8f3d7b80
pedro-maciel77 <ph385574@gmail.com>
2024-09-18 13:54:27 +0000

    criação de prompt

---
+++ atividade.js
@@ -0,0 +1,17 @@
+const nome = 'Pedro'
+console.log(nome)
+const idade = 18
+console.log(idade)
+console.log(`Olá meu nome é ${nome} e tenho ${idade} anos`)
+const cidade = 'Olinda' 
+console.log(`${nome} é de ${cidade}`)
+var num1 = 12
+var num2 = 22
+var soma = num1+num2
+console.log(soma)
+var soma = num1-num2
+console.log(soma)
+var soma = num1*num2
+console.log(soma)
+var soma = num1/num2
+console.log(soma)
---
+++ aula.js
@@ -0,0 +1,10 @@
+/*var prompt = require('prompt-sync')();
+
+let nome = prompt('Qual seu nome?')
+console.log(`Olá, ${nome}! Boas vindas a faculdade Senac`)*/
+
+var prompt = require('prompt-sync')();
+
+let atendimento = prompt('Qual produto voce quer?')
+console.log(`a ${atendimento} custa 2 reais`)
+
