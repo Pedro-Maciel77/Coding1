67d40cd88c1e725ae12f52a4a9b6895d8a3588cc
Pedro-Maciel77 <ph385574@gmail.com>
2024-09-25 10:39:52 -0300

    criei o repositório

--- atividade.js
+++ atividade.js
@@ -5,8 +5,8 @@ console.log(idade)
 console.log(`Olá meu nome é ${nome} e tenho ${idade} anos`)
 const cidade = 'Olinda' 
 console.log(`${nome} é de ${cidade}`)
-var num1 = 12
-var num2 = 22
+var num1 = 100
+var num2 = 20
 var soma = num1+num2
 console.log(soma)
 var soma = num1-num2
@@ -15,3 +15,8 @@ var soma = num1*num2
 console.log(soma)
 var soma = num1/num2
 console.log(soma)
+var salario = 2450
+var imposto = 0.15
+var pessoas = (3*150)
+ipr = salario-pessoas*imposto
+console.log(ipr)
